8a0be96b2340295713d132808743b19bae8e2cae
Christopher Bussick <bussick.christopher@gmail.com>
2024-05-28 16:00:21 +0200

    Add existing vscode folder

---
+++ .vscode/templates/WHFunctionComponent/___componentName___/___componentName___.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Box } from "@mui/material";
+import { ___Component_Name___Props } from "./___componentName___Interfaces";
+
+export const ___Component_Name___ =
+  ({}: ___Component_Name___Props): JSX.Element => {
+    return (
+      <Box sx={{ bgcolor: (t) => t.palette.primary.main }}>
+        Hey ___Component_Name___ 🚀
+      </Box>
+    );
+  };
