ffd6ec03fe4618b673e2cb1ea923a8bd7ebccc50
Christopher Bussick <bussick.christopher@gmail.com>
2024-06-06 00:17:21 +0200

    Restructure files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { MPMIContentWrapper } from "@/components/MPMIContentWrapper/MPMIContentWrapper";
-import { MPMIDashboardAdmin } from "@/components/MPMIDashboardAdmin/MPMIDashboardAdmin";
-import { MPMIDashboardStudent } from "@/components/MPMIDashboardStudent/MPMIDashboardStudent";
-import { MPMIDashboardTeacher } from "@/components/MPMIDashboardTeacher/MPMIDashboardTeacher";
+import { MPMIDashboardAdmin } from "@/components/dashboards/MPMIDashboardAdmin";
+import { MPMIDashboardStudent } from "@/components/dashboards/MPMIDashboardStudent";
+import { MPMIDashboardTeacher } from "@/components/dashboards/MPMIDashboardTeacher";
 import { useUser } from "@/firebase/useUser";
 import { MPMIUserRole } from "@/firebase/userRole";
 import { CircularProgress } from "@mui/material";
