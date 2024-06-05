d15c06ee8c6013e7aa1c80220c8501646c25a56c
Christopher Bussick <bussick.christopher@gmail.com>
2024-06-06 00:03:13 +0200

    Remove unused files

--- firebase/getUserUnlockedGlossaryEntries.ts
+++
@@ -1,6 +0,0 @@
-import { getUserCustomData } from "./getUserCustomData";
-
-export const getUserUnlockedGlossaryEntries = async (uid: string) => {
-  const userData = await getUserCustomData(uid);
-  return userData.data()?.unlockedGlossaryEntryIDs;
-};
