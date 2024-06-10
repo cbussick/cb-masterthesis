d903b1270eb68318dc91e4ac15e039a1b8d9541f
Christopher Bussick <bussick.christopher@gmail.com>
2024-06-10 13:43:23 +0200

    Clean up

--- ui/useSnackbar.tsx
+++ ui/useSnackbar.tsx
@@ -2,7 +2,5 @@ import { useContext } from "react";
 import { SnackbarContext } from "./SnackbarProvider";
 
 export const useSnackbar = () => {
-  const context = useContext(SnackbarContext);
-
-  return context;
+  return useContext(SnackbarContext);
 };
