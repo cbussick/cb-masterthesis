17546825f0c1fb35161c3633cd35cdff4d77b559
Christopher Bussick <bussick.christopher@gmail.com>
2024-06-15 11:27:13 +0200

    Remove unnecessary code

--- components/settings/CBSettings/CBTabPanel.tsx
+++ components/settings/CBSettings/CBTabPanel.tsx
@@ -3,13 +3,7 @@ import { CBTabPanelProps } from "./CBSettingsInterfaces";
 
 export const CBTabPanel = ({ children, value, index }: CBTabPanelProps) => {
   return (
-    <Box
-      role="tabpanel"
-      hidden={value !== index}
-      id={`tabpanel-${index}`}
-      aria-labelledby={`tab-${index}`}
-      sx={{ overflowY: "auto" }}
-    >
+    <Box hidden={value !== index} sx={{ overflowY: "auto" }}>
       {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
     </Box>
   );
