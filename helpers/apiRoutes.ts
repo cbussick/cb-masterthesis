70e22f48b85d02a3ccfda763a0254a9d31699483
Christopher Bussick <bussick.christopher@gmail.com>
2024-07-15 12:40:32 +0200

    Add missing file from previous commit

--- helpers/apiRoutes.ts
+++ helpers/apiRoutes.ts
@@ -3,9 +3,11 @@ const apiBaseUrl = "/api/";
 export enum CBAPIRoute {
   FreeformQuestion = "FreeformQuestion",
   DiNAsHint = "DiNAsHint",
+  Image = "Image",
 }
 
 export const apiRouteMap: Record<CBAPIRoute, string> = {
   [CBAPIRoute.FreeformQuestion]: `${apiBaseUrl}freeform-question`,
   [CBAPIRoute.DiNAsHint]: `${apiBaseUrl}dinas-hint`,
+  [CBAPIRoute.Image]: `${apiBaseUrl}image`,
 };
