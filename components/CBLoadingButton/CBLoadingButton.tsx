272f3a22ffb4a668793e8eaabbc1338d1e9e2046
Christopher Bussick <bussick.christopher@gmail.com>
2024-07-16 13:13:08 +0200

    Small adjustments (#48)
    * Change voice in order to avoid american accent
    
    * Keep loading button same width while loading
    
    * Add comment
    
    * Remove comma
    
    * Fix loadingbutton circle being wrongly aligned when no endicon present
    
    * Prevent speech from playing if dialog was closed while fetching

--- components/CBLoadingButton/CBLoadingButton.tsx
+++ components/CBLoadingButton/CBLoadingButton.tsx
@@ -1,4 +1,5 @@
-import { Button, CircularProgress } from "@mui/material";
+import { Button, buttonClasses, CircularProgress } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 import { CBLoadingButtonProps } from "./CBLoadingButtonInterfaces";
 
 export const CBLoadingButton = ({
@@ -6,9 +7,39 @@ export const CBLoadingButton = ({
   children,
   ...props
 }: CBLoadingButtonProps): JSX.Element => {
+  const ref = useRef<HTMLButtonElement>(null);
+
+  const [width, setWidth] = useState<number>();
+
+  useEffect(() => {
+    // Probably only works like this when the button starts with `isLoading` set to `true
+    setWidth(ref.current?.offsetWidth);
+  }, []);
+
   return (
-    <Button disabled={isLoading} {...props}>
-      {isLoading ? <CircularProgress size={24.5} /> : children}
+    <Button
+      disabled={isLoading}
+      {...props}
+      ref={ref}
+      sx={{
+        width,
+        [`& .${buttonClasses.endIcon}`]: {
+          // To keep the endIcon on the right side of the button while loading
+          marginLeft: isLoading ? "auto" : undefined,
+        },
+      }}
+    >
+      {isLoading ? (
+        <CircularProgress
+          size={24.5}
+          sx={{
+            // To center the Spinner while loading
+            marginLeft: props.endIcon ? "auto" : undefined,
+          }}
+        />
+      ) : (
+        children
+      )}
     </Button>
   );
 };
