06eac82be5880e10b66b514eb1565ff8c9962dfb
Christopher Bussick <bussick.christopher@gmail.com>
2024-08-28 15:12:11 +0200

    Delete users subcollections on delete

--- firebase-cloud-functions/functions/src/index.ts
+++ firebase-cloud-functions/functions/src/index.ts
@@ -18,12 +18,13 @@ export const deleteUserData = auth.user().onDelete(async (user) => {
   try {
     // Delete the document in the 'users' collection
     const customDataRef = db.collection("users").doc(uid);
-    await customDataRef.delete();
+    await db.recursiveDelete(customDataRef);
     logger.info(`Deleted custom data for user: ${uid}`);
 
     // Delete the document in the 'topicWorldProgress' collection
     const topicWorldProgressRef = db.collection("topicWorldProgress").doc(uid);
     await topicWorldProgressRef.delete();
+
     logger.info(`Deleted topicWorldProgress for user: ${uid}`);
   } catch (error) {
     logger.error(`Error deleting user data for user: ${uid}`, error);
